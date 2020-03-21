9306d7a746491c94aa17d77d70401ab1b15bc50c
DongXin9 <2627405095@qq.com>
2020-03-21 17:47:46 +0800

    component

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(function(){
     ["Python", 3, "升", "+1.41%"],
     ["C++", 4, "降", "-2.58%"],
     ["C#", 5, "升", "+2.07%"],
-    ["Visual Basic .NET", 6, "升", "-1.17%"],
+    ["Visual Basic .NET", 6, "降", "-1.17%"],
     ["JavaScript", 7, "降", "-0.85%"] 
   ];
   var option = {
@@ -43,14 +43,18 @@ $(function(){
   };
   myChart.setOption(option);
   new Handsontable(excel,{
-      data: exceldata, 
+      data: exceldata,
+      className:'htCenter htMiddle',
       colWidths:'175px',
+      rowHeights:40,
       colHeaders:[
         '语言名称',
         '排名',
         '升降',
         '变化幅度'
-      ]
+      ],
+      manualColumnResize: true,
+      manualRowResize: true
   })
 
 })
